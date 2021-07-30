8849dee82b048f0367d3a211b854ae317eff7a3e
ykwimk <ykwimk@gmail.com>
2021-07-30 12:51:49 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/jPcEJGtsOFvIJ-rGMI-M2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-20259bf9180ed189342c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f5da0296e50ca48cfada.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
